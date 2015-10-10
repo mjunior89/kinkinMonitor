0c8a9da1d30b72ced2c40bf8ff5a491cbc9bd110
mario.araujo <mario.araujo@consultores.reconcavo.org.br>
2015-10-10 20:33:04 -0300

    Adição da biblioteca JDBC do postgresql

--- src/br/hue/hue/inf008/kinkinmonitor/Main.java
+++ src/br/hue/hue/inf008/kinkinmonitor/Main.java
@@ -1,9 +1,12 @@
 package br.hue.hue.inf008.kinkinmonitor;
 
+import br.hue.hue.inf008.kinkinmonitor.persistence.JDBCFactoryManager;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        JDBCFactoryManager factory = new JDBCFactoryManager();
+        factory.executeStatement("select now()");
     }
 
 }

f0148f217f1ccf5059c225fb203c97bf0285cc61
serginhopib <>
2015-10-19 21:54:37 -0200

    Criação das telas

--- src/br/hue/hue/inf008/kinkinmonitor/model/AreaMonitorada.java
+++ src/br/hue/hue/inf008/kinkinmonitor/model/AreaMonitorada.java
@@ -1,70 +1,70 @@
 package br.hue.hue.inf008.kinkinmonitor.model;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
 
 public class AreaMonitorada {
 
-	public static final String SEQUENCE = "SQ_AREA_MONITORADA";
+    public static final String SEQUENCE = "SQ_AREA_MONITORADA";
 
-	private int id;
-	private String nome;
-	private List<UnidadeMonitora> unidades = new ArrayList<>();
+    private int id;
+    private String nome;
+    private Collection<UnidadeMonitora> unidades = new ArrayList<>();
 
-	public AreaMonitorada() {
-	}
+    public AreaMonitorada() {
+    }
 
-	public AreaMonitorada(int id, String nome) {
-		this.id = id;
-		this.nome = nome;
-	}
+    public AreaMonitorada(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
 
-	public int getId() {
-		return id;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public String getNome() {
-		return nome;
-	}
+    public String getNome() {
+        return nome;
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	public List<UnidadeMonitora> getUnidades() {
-		return unidades;
-	}
+    public Collection<UnidadeMonitora> getUnidades() {
+        return unidades;
+    }
 
-	public void setUnidades(List<UnidadeMonitora> unidades) {
-		this.unidades = unidades;
-	}
+    public void setUnidades(Collection<UnidadeMonitora> unidades) {
+        this.unidades = unidades;
+    }
 
-	@Override
-	public String toString() {
-		return "Área Monitorada Nº " + id + ", " + this.getNome();
-	}
+    @Override
+    public String toString() {
+        return "Área Monitorada Nº " + id + ", " + this.getNome();
+    }
 
-	@Override
-	public int hashCode() {
-		int hash = 7;
-		hash = (31 * hash) + this.getId();
-		return hash;
-	}
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = (31 * hash) + this.getId();
+        return hash;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (!(obj instanceof AreaMonitorada)) {
-			return false;
-		}
-		AreaMonitorada other = (AreaMonitorada) obj;
-		if (this.getId() == 0) {
-			return this == other;
-		} else {
-			return this.getId() == other.getId();
-		}
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof AreaMonitorada)) {
+            return false;
+        }
+        AreaMonitorada other = (AreaMonitorada) obj;
+        if (this.getId() == 0) {
+            return this == other;
+        } else {
+            return this.getId() == other.getId();
+        }
+    }
 }

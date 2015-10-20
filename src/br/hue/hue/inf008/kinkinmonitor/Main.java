f74d42bc8dcb2fe2952d341bbe2cc1224c7b93ec
mjunior89 <>
2015-10-20 00:53:04 -0300

    Alterações nas configurações

--- src/br/hue/hue/inf008/kinkinmonitor/Main.java
+++ src/br/hue/hue/inf008/kinkinmonitor/Main.java
@@ -1,12 +1,21 @@
 package br.hue.hue.inf008.kinkinmonitor;
 
 import br.hue.hue.inf008.kinkinmonitor.persistence.DataSource;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main {
 
 	public static void main(String[] args) {
 		DataSource ds = new DataSource();
-		ds.initDataBase();
+		try {
+			ds.initDataBase();
+		} catch (SQLException ex) {
+			Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+		}
+		IntegrationSPEC tests = new IntegrationSPEC();
+
 	}
 
 }
